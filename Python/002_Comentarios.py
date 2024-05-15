60a7aa327c2a946f600365e7cc6b01ec7e25d957
Agdc10 <95agdc@gmail.com>
2024-05-15 23:27:24 +0200

    Version 1.0

---
+++ Python/002_Comentarios.py
@@ -0,0 +1,8 @@
+# Los comentarios son anotaciones que pondremos en nuestro código que el programa no va a tener en cuenta.
+# existen dos tipos de comentarios:
+
+# Esto es un comentario de una linea
+
+"""ESto es un comentario
+que me va a ocupar
+varias lineas"""
---
+++ Python/002_Operadores.py
@@ -0,0 +1,6 @@
+# Módulo. Nos devuelve el resto de una división:
+
+n_numerador = 85
+n_denominador = 9
+n_resto = n_numerador%n_denominador
+print("El resto de dividir" , n_numerador , "entre" , n_denominador , "es" , n_resto)
